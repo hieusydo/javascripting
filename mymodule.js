ee847edaf046a79e4ceb328faa1dc65cbffd1ab3
hieusydo <hieusydo@gmail.com>
2015-10-03 14:13:53 -0400

    Modular

--- mymodule.js
+++ mymodule.js
@@ -1,14 +1,33 @@
 //by default, var exports = module.exports = {}
+//console.log('module hello');
 
-console.log('Hi');
+var fs = require('fs');
+var path = require('path');
 
-exports.printName = function() {
-	console.log("Hi, this is Hieu");
-};
+module.exports = function(dirName, ext, callback) {
+	fs.readdir(dirName, function(err, list) {
+		if (err) return callback(err); 
 
-exports.printGreeting = function() {
-	console.log("Yay my first module is working");
-};
+		var results = [];
+		list.forEach(function(file) {
+			if (path.extname(file) == ('.'+ ext)) {
+	            results.push(file);			
+			}
+		})
+
+		return callback(null, results);
+	});
+}
+
+
+
+// exports.printName = function() {
+// 	console.log("Hi, this is Hieu");
+// };
+
+// exports.printGreeting = function() {
+// 	console.log("Yay my first module is working");
+// };
 
 // var reassign = module.exports; ==> this reassign module.exports to a string, no longer var exports 
 // Fix: reassign.printName = function() {
--- program.js
+++ program.js
@@ -1,14 +1,10 @@
-var fs = require('fs');
-var p = require('path');
-
-var path = process.argv[2];
+var dirName = process.argv[2];
 var ext = process.argv[3];
 
-fs.readdir(path, function(err, list) {
-	if (err) throw err; 
-	for (var i = 0; i < list.length; i++) {
-		if (p.extname(list[i]) == ('.'+ ext)) {
-            console.log(list[i]);			
-		}
-	}
+var mymodule = require('./mymodule.js')
+
+mymodule(dirName, ext, function(err, results) {
+	results.forEach(function(file) {
+		console.log(file);
+	})
 });
